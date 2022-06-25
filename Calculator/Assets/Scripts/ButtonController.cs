c3b3522fa7cd1a2476c3f09a5f5a55cb73c48fca
WFongL <105300336+WFongL@users.noreply.github.com>
2022-06-25 09:00:11 +0300

    Calculator

---
+++ Calculator/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public string value;
+    public DisplayController displayController;
+
+    void Start()
+    {
+        displayController = GameObject.Find("DisplayPanel").GetComponent<DisplayController>();
+    }
+
+    public void AppendValueToDisplay()
+    {
+        displayController.UpdateDisplayText(value);
+    }
+
+    public void Clear() // C 
+    {
+        displayController.ClearDisplay();
+    }
+
+    public void RemoveDigit() // < 
+    {
+        displayController.RemoveDigitDisplay();
+    }
+
+    public void ClearCurrentEntry() // CE 
+    {
+        displayController.ClearCurrentEntryDisplay();
+    }
+
+    public void EvaluateEquation() // = 
+    {
+        displayController.DisplayAnswer();
+    }
+}
